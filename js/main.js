a8bda7bb03cb5948b14ce0afa346f8eb78d55396
jonathon Mullins <jonathonm93@gmail.com>
2020-12-10 10:48:31 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 30000); // Change image every 3 seconds
-}
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 1000,
+        cssEase: 'ease-out',
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
